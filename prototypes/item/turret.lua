9547d7538e0b3f18c40546f0f83f00439062ddb4
lemondrop81 <hacker814@hotmail.com>
2021-09-07 11:15:31 -0500

    Create item for gattling gun turret

---
+++ prototypes/item/turret.lua
@@ -0,0 +1,12 @@
+data:extend({
+    {
+        type = "item",
+        name = "gattling_gun",
+        icon = "__horsemen-armor__/graphics/icons/gattling_gun.png",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "energy-pipe-distribution",
+        place_result = "defensive-structure",
+        stack_size = 50
+    },
+}
+)
