65582b38aadec3b2e38fa323eb76859e1a4c3115
lemondrop81 <hacker814@hotmail.com>
2021-09-07 10:48:56 -0500

    Create underground belt research

--- prototypes/technology/belt.lua
+++ prototypes/technology/belt.lua
@@ -37,3 +37,43 @@ data:extend({
 	}
 }
 )
+
+-- Research the 2fast-2furious underground belt
+local two_fast_belt_underground = table.deepcopy(data.raw["underground-belt"]["express-underground-belt"]) -- copy the table that defines the heavy armor item into the fireArmor variable
+
+two_fast_belt_underground.icons = {
+  {
+    icon = two_fast_belt.icon,
+    tint = {r=0.9,g=0.2,b=0.2,a=0.3}
+  },
+}
+
+data:extend({
+	{
+		type = "technology",
+		name = "2fast-2furious-underground",
+		icon_size = 64,
+		icon = two_fast_belt_underground.icon,
+		effects =  {
+			{
+				type = "unlock-recipe",
+				recipe = "2fast-2furious-underground-craft"
+			},
+		},
+		prerequisites = {"war-armor", "pestilence-armor", "famine-armor"},
+		unit = {
+			count = 100,
+			ingredients = {
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"chemical-science-pack", 1},
+				{"military-science-pack", 2},
+				{"production-science-pack", 1},
+				{"utility-science-pack", 2}
+			},
+			time = 30
+		},
+		order = "g-c-b"
+	}
+}
+)
