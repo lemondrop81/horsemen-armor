2045610006f8f3975514803c5a776069845e679f
lemondrop81 <hacker814@hotmail.com>
2021-09-20 10:04:18 -0500

    change waterrtube boiler recipe

--- prototypes/recipe/boiler.lua
+++ prototypes/recipe/boiler.lua
@@ -32,8 +32,9 @@ data:extend({
         enabled = false,
         energy_required = 1,
         ingredients = {
-            { "steel-pipe", 10}, 
-            { "steel-furnace", 3}, 
+            { "concrete", 10}, 
+            { "heat-pipe", 10}, 
+            { "electic-furnace", 3}, 
         },
         result = "Watertube Boiler",
         requester_paste_multiplier = 15
