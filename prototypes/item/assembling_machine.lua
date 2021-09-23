437e9498c25818281ab3906c9bc0ae96abd14b39
lemondrop81 <hacker814@hotmail.com>
2021-09-23 09:41:44 -0500

    Chnage default icon

--- prototypes/item/assembling_machine.lua
+++ prototypes/item/assembling_machine.lua
@@ -2,7 +2,7 @@ data:extend({
     {
         type = "item",
         name = "hephaestus_builders",
-        icon = "__base__/graphics/icons/furnace.png",
+        icon = "__base__/graphics/icons/assembling-machine-3.png",
         icon_size = 64, icon_mipmaps = 4,
         subgroup = "production-machine",
         place_result = "hephaestus_builders",
