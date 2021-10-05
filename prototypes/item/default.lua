05b4f7e89479c7537dbc6b3862f2ca07c85e659f
lemondrop81 <hacker814@hotmail.com>
2021-10-05 09:36:11 -0500

    Update default items to custom ones

--- prototypes/item/default.lua
+++ prototypes/item/default.lua
@@ -4,6 +4,26 @@
 data.raw["electric-pole"]["big-electric-pole"].next_upgrade = "230kV_pole"
 data.raw["electric-pole"]["big-electric-pole"].fast_replaceable_group = "electric-pole"
 
---Default big electric poles to upgrade to 230kV poles
+--Default boiler to upgrade to scotch marine boiler
 data.raw["boiler"]["boiler"].next_upgrade = "Scotch Marine Boiler"
 data.raw["boiler"]["boiler"].fast_replaceable_group = "boiler"
+
+--Default assembling machine to upgrade to hephaestus_builders
+data.raw["assembling-machine"]["assembling-machine-3"].next_upgrade = "hephaestus_builders"
+data.raw["assembling-machine"]["assembling-machine-3"].fast_replaceable_group = "assembling-machine"
+
+--Default express belt to upgrade to 2fast-2furious
+data.raw["transport-belt"]["express-transport-belt"].next_upgrade = "2fast-2furious"
+data.raw["transport-belt"]["express-transport-belt"].fast_replaceable_group = "transport-belt"
+
+--Default express belt to upgrade to 2fast-2furious
+data.raw["underground-belt"]["express-underground-belt"].next_upgrade = "2fast-2furious-underground"
+data.raw["underground-belt"]["express-underground-belt"].fast_replaceable_group = "transport-belt"
+
+--Default electric drill to upgrade to hephaestus_drill
+data.raw["mining-drill"]["electric-mining-drill"].next_upgrade = "hephaestus_drill"
+data.raw["mining-drill"]["electric-mining-drill"].fast_replaceable_group = "mining-drill"
+
+--Default steam turbine to upgrade to super turbine
+data.raw["generator"]["steam-turbine"].next_upgrade = "superTurbine"
+data.raw["generator"]["steam-turbine"].fast_replaceable_group = "steam-engine"
