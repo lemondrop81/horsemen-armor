11ddb41d11056601816fbc38d3ffd2384798d98c
lemondrop81 <hacker814@hotmail.com>
2021-10-25 10:37:41 -0500

    Update fuelcell recipe

--- prototypes/recipe/fuelcell.lua
+++ prototypes/recipe/fuelcell.lua
@@ -5,7 +5,10 @@ data:extend(
             type = "recipe",
             enabled = true,
             name = "magma-fuel-cell",
-            ingredients = {{"iron-plate", 1}},
+            ingredients = {
+                { "beryllium-plate", 5}, 
+                {type="fluid", name="magma", amount=10} 
+            },
             result_count = 4,
             result = "magma-fuel-cell"
         }
